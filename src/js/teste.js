6ea5a4172569c08304affdd7b67be5758e93f2db
Ysmael Marques <ysmaelmarkss@gmail.com>
2022-10-24 16:35:41 -0300

    adicionando coisas

---
+++ src/js/teste.js
@@ -0,0 +1,35 @@
+const url = "http://ancient-plateau-45399.herokuapp.com"
+const search = document.getElementById('search')
+const matchList = document.getElementById('match-list')
+/* const limit = 3;
+const page = 1; */
+
+const searchNames = async searchText =>{
+    const res = await fetch ('http://api-clima-resilia.herokuapp.com');
+    const cidades = await res.json();
+
+    let matches = cidades.filter(cidade => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return cidade?.nome?.match(regex)
+    });
+
+    if(searchText.length === 0){
+        matches = []
+        matchList.innerHTML = ''
+    }
+
+    outputHtml(matches);
+}
+
+    const outputHtml = matches =>{
+        if(matches.length > 0){
+            const html = matches.map(match => `
+                <div class="card card-body mb-1">
+                    <h4>${match.nome}</h4>
+                    <small>${match.ddd}</small>
+                </div>`)
+                .join('')
+            matchList.innerHTML = html
+        }
+    }
+search.addEventListener('input', () => searchNames(search.value))
